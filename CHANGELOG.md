1b0eceb8c4eca507a0f8e4de469c4ee18153b32a
niku <niku@niku.name>
2013-12-31 05:59:42 +0900

    Login with ssh_keys in the 'data_bag'

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+user-ng CHANGELOG
+===================
+
+This file is used to list changes made in each version of the user-ng cookbook.
+
+0.1.0
+-----
+- [niku] - Initial release of user-ng
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: user-ng
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+data_bag('users').each do |id|
+  u = data_bag_item('users', id)
+  username = u['id']
+  homedir = "/home/#{username}"
+
+  user username do
+    supports manage_home: true
+    home homedir
+    shell u['shell'] || '/bin/bash'
+  end
+
+  if u['ssh_keys']
+    sshdir = "#{homedir}/.ssh"
+
+    directory sshdir do
+      owner username
+      group username
+      mode '0700'
+    end
+
+    template "#{sshdir}/authorized_keys" do
+      source 'authorized_keys.erb'
+      owner username
+      group username
+      mode '0400'
+      variables ssh_keys: u['ssh_keys']
+    end
+  end
+
+  if u['sudo_privilege']
+    template "/etc/sudoers.d/#{username}" do
+      source 'sudo_privilege.erb'
+      variables username: username, sudo_privilege: u['sudo_privilege']
+      mode '0440'
+    end
+  end
+end
